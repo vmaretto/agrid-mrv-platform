4359132ccfc78551e88fb9bf8ea910dfb643b309
vmaretto <vmaretto@gmail.com>
2026-06-05 12:37:34 +0200

    Configure multi page Vite build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        farmer: 'farmer/index.html',
+        admin: 'admin/index.html',
+        stakeholder: 'stakeholder/index.html',
+      },
+    },
+  },
+});
